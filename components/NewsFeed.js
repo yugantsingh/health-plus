eaaf5573fc9086129ca3ef7607c1856e7475dc96
Yugant Singh <yugantx@gmail.com>
2022-04-08 15:17:29 +0530

    completed feed initial config

---
+++ components/NewsFeed.js
@@ -0,0 +1,11 @@
+import News from "./News";
+export default function NewsFeed() {
+  return (
+    <div>
+      <News />
+      <News />
+      <News />
+      <News />
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import Image from "next/image";
+import Feed from "../components/Feed";
 import Header from "../components/Header";
 import styles from "../styles/Home.module.css";
 
@@ -10,9 +10,16 @@ export default function Home() {
         <title>Health+</title>
         <meta name="description" content="Health+ - Home Page" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;400;700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
 
       <Header />
+      <Feed />
     </div>
   );
 }
