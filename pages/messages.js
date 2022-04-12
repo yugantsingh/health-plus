eabffb8d3b3e2dc6d4499e8c4cd15339713e5a3f
Yugant Singh <yugantx@gmail.com>
2022-04-12 23:50:22 +0530

    init messages routing

--- pages/messages.js
+++ pages/messages.js
@@ -4,9 +4,15 @@ function messages({ user }) {
   return (
     <div>
       <Header />
-      <h1>Protected Route Messages</h1>
-      <p>Welcome {user.name}</p>
-      <p>{user.email}</p>
+      <div className="grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl mx-auto">
+        <section className="col-span-1">
+          <div>Hello</div>
+        </section>
+
+        <section className="col-span-2">
+          <div>Small Hello</div>
+        </section>
+      </div>
     </div>
   );
 }
