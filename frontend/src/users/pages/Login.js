13525e355c01c4d31f5e7f25c1242ff88ddb44e0
Yugant Singh <yugantx@gmail.com>
2022-04-21 19:04:35 +0530

    add

--- frontend/src/users/pages/Login.js
+++ frontend/src/users/pages/Login.js
@@ -21,14 +21,14 @@ const Login =() =>{
     var password = document.getElementById('pass').value;
     try {
       const responseData = await sendRequest(
-        '/api/users/login',
-        'POST',
+        "https://health-plus-vit.herokuapp.com/api/users/login",
+        "POST",
         JSON.stringify({
           email: email,
-          password: password
+          password: password,
         }),
         {
-          'Content-Type': 'application/json'
+          "Content-Type": "application/json",
         }
       );
       auth.login(responseData.user.email, responseData.token);
