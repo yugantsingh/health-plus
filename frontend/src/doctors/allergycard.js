47374490a51e8167d8e93343bc16e70e110aa86a
Yugant Singh <yugantx@gmail.com>
2022-04-22 08:14:41 +0530

    commit

--- frontend/src/doctors/allergycard.js
+++ frontend/src/doctors/allergycard.js
@@ -3,30 +3,27 @@ import Card from "react-bootstrap/Card";
 import Akira from "../jj.jpg";
 import "../css/DoctorListCard.css";
 
-
 const AllergyCard = (props) => {
-    return (
-      <div>
-        <Card border="info" className="DoctorListCards">
-          <br />
-          <font className="DoctorIcon">
-            <i className="fas fa-bacteria fa-9x"></i>
-          </font>
-          <Card.Body>
-            <Card.Title style={{ fontSize: "2rem", color: "#195a65" }}>
-              Report
-            </Card.Title>
-            <Card.Text>
-              Title: {props.title}
-              Place:{props.place}
-              Date:{props.date}
-              Description:{props.description}
-              Medication:{props.meds}
-            </Card.Text>
-          </Card.Body>
-        </Card>
-      </div>
-    );
+  return (
+    <div>
+      <Card border="info" className="DoctorListCards">
+        <br />
+        <font className="DoctorIcon">
+          <i className="fas fa-books-medical fa-9x"></i>
+        </font>
+        <Card.Body className="Allergy">
+          <Card.Title style={{ color: "#195a65" }}>Report</Card.Title>
+          <Card.Text>
+            <div>Title: {props.title}</div>
+            <div>Place:{props.place}</div>
+            <div>Date:{props.date}</div>
+            <div>Description:{props.description}</div>
+            <div>Medication:{props.meds}</div>
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 };
 
 export default AllergyCard;
